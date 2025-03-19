3a12388e8e1bbdaa9bdcf9346d7647d51285b404
Tetiana Lykholot <tetiana_lykholot@epam.com>
2025-03-19 11:08:47 +0100

    Add changes

--- src/test/java/taskParallelExecution/LoginPage.java
+++ src/test/java/taskParallelExecution/LoginPage.java
@@ -1,12 +1,16 @@
 package taskParallelExecution;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage extends WriteToFile {
+import java.io.IOException;
 
-    @FindBy(id = "username")
+public class LoginPage extends BaseTest {
+
+    @FindBy(id = "user-name") // Check the exact ID in the application
     private WebElement usernameInput;
 
     @FindBy(id = "password")
@@ -15,8 +19,18 @@ public class LoginPage extends WriteToFile {
     @FindBy(id = "login-button")
     private WebElement loginButton;
 
+    // Constructor to initialize the PageFactory elements
+    public LoginPage() {
+        WebDriver driver =  getDriver();
+        if(driver == null){
+            throw new IllegalStateException("WebDriver is not initialized.");
+        }
+        // Initialize the web elements
+        PageFactory.initElements(driver, this);
+    }
 
     public void enterUsername(String username) {
+        if (usernameInput == null) throw new NullPointerException("Username input element is null!");
         usernameInput.sendKeys(username);
     }
 
@@ -28,24 +42,11 @@ public class LoginPage extends WriteToFile {
         loginButton.click();
     }
 
-
-    public void login(String username, String password) {
+    public void login(String username, String password) throws IOException {
+        getDriver().get("https://www.saucedemo.com/");
         enterUsername(username);
         enterPassword(password);
         clickLoginButton();
-        String uniqueVariable = "Variable_" + uniqueIdGenerator.getAndIncrement();
-        threadLocalForUsers.set(uniqueVariable);
-        String logMessage =
-                " | Thread: " + Thread.currentThread().getName() +
-                " | Username: " + username +
-                " | UniqueVariable: " + threadLocalForUsers.get();
-
-        writeToFile(logMessage);
+        writeUserEmailToFile(username);
     }
-
-
-
-
-
-
 }
--- src/test/java/taskParallelExecution/ParallelExecutionForClass.java
+++ src/test/java/taskParallelExecution/ParallelExecutionForClass.java
@@ -2,11 +2,13 @@ package taskParallelExecution;
 
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 
 public class ParallelExecutionForClass extends LoginPage {
 
     @Test (dataProvider = "loginDataProvider", dataProviderClass = DataProviderUsers.class)
-    public void loginToTheTestPage(String email, String password) {
+    public void loginToTheTestPage(String email, String password) throws IOException {
          login(email, password);
     }
     }
